1db285872f8c9173db00a33f9ede29f55af31cc2
jkuehner <jochen.kuehner@kardex.com>
2021-05-08 12:43:38 +0200

    add patternfly

--- src/appShell.ts
+++ src/appShell.ts
@@ -168,6 +168,7 @@ export class AppShell extends BaseCustomWebComponentConstructorAppend {
     serviceContainer.register('elementsService', new JsonFileElementsService('demo', './src/elements-demo.json'));
     serviceContainer.register('elementsService', new JsonFileElementsService('wired', './src/elements-wired.json'));
     serviceContainer.register('elementsService', new JsonFileElementsService('elix', './src/elements-elix.json'));
+    serviceContainer.register('elementsService', new JsonFileElementsService('patternfly', './src/elements-pfe.json'));
     serviceContainer.register('elementsService', new JsonFileElementsService('mwc', './src/elements-mwc.json'));
     serviceContainer.register('elementsService', new JsonFileElementsService('native', './node_modules/@node-projects/web-component-designer/src/config/elements-native.json'));
 
