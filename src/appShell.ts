98614d1a10e4cd2af3b2079041ff77fa05feecfe
jkuehner <jochen.kuehner@kardex.com>
2021-06-02 17:15:49 +0200

    add paint

--- src/appShell.ts
+++ src/appShell.ts
@@ -172,6 +172,7 @@ export class AppShell extends BaseCustomWebComponentConstructorAppend {
   private async _setupServiceContainer() {
     serviceContainer.register('elementsService', new JsonFileElementsService('demo', './src/elements-demo.json'));
     await OldCustomElementsManifestLoader.loadManifest(serviceContainer, '@spectrum-web-components/button', { name: '@spectrum' });
+    serviceContainer.register('elementsService', new JsonFileElementsService('paint', './src/elements-paint.json'));
     serviceContainer.register('elementsService', new JsonFileElementsService('wired', './src/elements-wired.json'));
     serviceContainer.register('elementsService', new JsonFileElementsService('elix', './src/elements-elix.json'));
     serviceContainer.register('elementsService', new JsonFileElementsService('patternfly', './src/elements-pfe.json'));
