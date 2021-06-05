bff1bb5d1be84ea483daae4dc7056fe524e41499
jkuehner <jochen.kuehner@kardex.com>
2021-06-05 12:22:56 +0200

    bugfixes

--- src/demo/DemoElement.ts
+++ src/demo/DemoElement.ts
@@ -24,6 +24,7 @@ export class DemoElement extends BaseCustomWebComponentConstructorAppend {
     <div>[[this.specialText]]</div>
     <div>[[this.enumProperty]]</div>
     <div id="cnt" style="background: lightblue;">Test JS Access</div>
+    <slot></slot>
     <!--<div>[[DemoEnum[this.enumProperty] ]]</div>-->
   </div>`
 
