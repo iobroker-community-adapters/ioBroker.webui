e2d62115a6e4147fa2dacfd34e0abc15b822ce7f
jogibear9988 <jochen.kuehner@gmx.de>
2021-12-16 23:10:03 +0100

    move code again

--- www/dist/services/CustomBindableObjectsService.d.ts
+++
@@ -1,5 +0,0 @@
-import { IBindableObjectsService, IBindableObject } from "@node-projects/web-component-designer";
-export declare class CustomBindableObjectsService implements IBindableObjectsService {
-    getBindableObject(fullName: string): Promise<IBindableObject>;
-    getBindableObjects(parent?: IBindableObject): Promise<IBindableObject[]>;
-}
