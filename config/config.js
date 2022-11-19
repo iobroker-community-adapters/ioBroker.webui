c3796ff3b13f93812d6190d6bfd272ae5e3e3cc0
jkuehner <jochen.kuehner@kardex.com>
2022-11-19 22:28:46 +0100

    update socket url

--- config/config.js
+++ config/config.js
@@ -2,3 +2,4 @@ window.iobrokerHost = window.location.hostname;
 window.iobrokerPort = window.location.port;
 window.iobrokerWebuiRootUrl = window.location.protocol + '//' + window.iobrokerHost + ':' + window.iobrokerPort + '/webui/';
 window.iobrokerSocketScriptUrl = '../lib/js/socket.io.js';
+window.socketUrl = window.location.protocol + '//' + window.iobrokerHost + ':' + window.iobrokerPort + '/';
