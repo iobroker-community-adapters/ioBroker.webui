e6bfe4e10107c39a8904edc4112cd38bfa00e512
Jochen Kuehner <jochen.kuehner@gmx.de>
2023-08-06 22:32:26 +0200

    hack cause of not workin socket

--- config/config.js
+++ config/config.js
@@ -2,4 +2,8 @@ window.iobrokerHost = window.location.hostname;
 window.iobrokerPort = window.location.port;
 window.iobrokerWebuiRootUrl = window.location.protocol + '//' + window.iobrokerHost + ':' + window.iobrokerPort + '/webui/';
 window.iobrokerSocketScriptUrl = '../lib/js/socket.io.js';
+//hack, webui socket does not work atm...
+window.iobrokerPort = 8081;
+window.iobrokerSocketScriptUrl = window.location.protocol + '//' + window.iobrokerHost + ':' + window.iobrokerPort + '/lib/js/socket.io.js';
+//end of hack
 window.socketUrl = window.location.protocol + '//' + window.iobrokerHost + ':' + window.iobrokerPort + '/';
