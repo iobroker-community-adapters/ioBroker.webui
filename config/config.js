489006bf4d0414e06450d75062b8df17db483145
Jochen Kuehner <jochen.kuehner@gmx.de>
2023-08-10 08:52:59 +0200

    undo config hack for admin usage

--- config/config.js
+++ config/config.js
@@ -3,7 +3,7 @@ window.iobrokerPort = window.location.port;
 window.iobrokerWebuiRootUrl = window.location.protocol + '//' + window.iobrokerHost + ':' + window.iobrokerPort + '/webui/';
 window.iobrokerSocketScriptUrl = '../lib/js/socket.io.js';
 //hack, webui socket does not work atm...
-window.iobrokerPort = 8081;
-window.iobrokerSocketScriptUrl = window.location.protocol + '//' + window.iobrokerHost + ':' + window.iobrokerPort + '/lib/js/socket.io.js';
+//window.iobrokerPort = 8081;
+//window.iobrokerSocketScriptUrl = window.location.protocol + '//' + window.iobrokerHost + ':' + window.iobrokerPort + '/lib/js/socket.io.js';
 //end of hack
 window.socketUrl = window.location.protocol + '//' + window.iobrokerHost + ':' + window.iobrokerPort + '/';
