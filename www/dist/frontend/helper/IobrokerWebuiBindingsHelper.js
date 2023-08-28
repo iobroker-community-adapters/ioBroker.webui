04608ed37a39d91da199d7f459c1eff8c83dc649
Jochen Kuehner <jochen.kuehner@gmx.de>
2023-08-28 17:08:21 +0200

    compiled code

--- www/dist/frontend/helper/IobrokerWebuiBindingsHelper.js
+++ www/dist/frontend/helper/IobrokerWebuiBindingsHelper.js
@@ -168,7 +168,7 @@ export class IobrokerWebuiBindingsHelper {
                 let cb = (id, value) => IobrokerWebuiBindingsHelper.handleValueChanged(element, binding, value.val, valuesObject, i);
                 unsubscribeList.push(cb);
                 iobrokerHandler.connection.subscribeState(s, cb);
-                iobrokerHandler.connection.getState(s).then(x => IobrokerWebuiBindingsHelper.handleValueChanged(element, binding, x.val, valuesObject, i));
+                iobrokerHandler.connection.getState(s).then(x => IobrokerWebuiBindingsHelper.handleValueChanged(element, binding, x?.val, valuesObject, i));
                 if (binding[1].twoWay) {
                     for (let e of binding[1].events) {
                         const evt = element[e];
