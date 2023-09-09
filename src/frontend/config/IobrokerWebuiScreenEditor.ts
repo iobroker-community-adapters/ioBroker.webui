659339db5081c5610098e662cb3ccb9745a8d917
Jochen Kuehner <jochen.kuehner@gmx.de>
2023-09-09 02:08:32 +0200

    fix saveing

--- src/frontend/config/IobrokerWebuiScreenEditor.ts
+++ src/frontend/config/IobrokerWebuiScreenEditor.ts
@@ -112,7 +112,7 @@ export class IobrokerWebuiScreenEditor extends BaseCustomWebComponentConstructor
 
     async executeCommand(command: IUiCommand) {
         if ((<string>command.type) == 'save') {
-            let html = this.documentContainer.designerView.getHTML();
+            let html = this.documentContainer.content;
             let style = this.documentContainer.additionalData.model.getValue();
             let script = this.scriptModel.getValue();
             if (this._type == 'screen') {
