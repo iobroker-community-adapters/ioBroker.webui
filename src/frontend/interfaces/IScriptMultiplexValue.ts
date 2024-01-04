8a1e106a28dda3d991231a0ff90fd314c7abdb2a
jogibear9988 <jochen.kuehner@gmx.de>
2024-01-04 07:19:48 +0100

    fix order

--- src/frontend/interfaces/IScriptMultiplexValue.ts
+++ src/frontend/interfaces/IScriptMultiplexValue.ts
@@ -1,9 +1,9 @@
 export class IScriptMultiplexValue {
+    source: 'signal' | 'property' | 'event';
     /**
      * Name of the ioBroker object or the property of the component
      * or for example in a event : srcElement.value to get the value of a input wich raises the event
      * @TJS-format signal
      */
     name: string;
-    source: 'signal' | 'property' | 'event'
 }
