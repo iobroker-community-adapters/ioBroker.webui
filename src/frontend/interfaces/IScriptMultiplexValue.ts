2a19f9685487a6e4de34d0ce3e5d4f6533e96ebc
jogibear9988 <jochen.kuehner@gmx.de>
2024-01-06 09:12:56 +0100

    more comments

--- src/frontend/interfaces/IScriptMultiplexValue.ts
+++ src/frontend/interfaces/IScriptMultiplexValue.ts
@@ -1,4 +1,10 @@
 export class IScriptMultiplexValue {
+     /**
+     * signal - read the value from a IOB object
+     * property - read the value from a property of the customControl (not usable in screens)
+     * event - read the value of a property of the event object
+     * @TJS-format signal
+     */
     source: 'signal' | 'property' | 'event';
     /**
      * Name of the ioBroker object or the property of the component
